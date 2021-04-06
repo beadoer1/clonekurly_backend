10b6df84e32a15ac632a3521529e128277e84d89
beadoer1 <beadoer1@gmail.com>
2021-04-06 21:44:38 +0900

    build.gradle 내 Jsoup,셀레니움 설정정보 삭제

--- src/main/java/com/sparta/clonekurly/repository/ProductRepository.java
+++ src/main/java/com/sparta/clonekurly/repository/ProductRepository.java
@@ -1,10 +1,9 @@
 package com.sparta.clonekurly.repository;
 
 import com.sparta.clonekurly.model.Product;
-import com.sparta.clonekurly.model.User;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
 }
