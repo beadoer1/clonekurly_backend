ee8d142920f64d901833b93eadfdea6c10a5e01c
sezeom <sejeong0516@gmail.com>
2021-04-06 19:50:21 +0900

    join, login !

---
+++ src/main/java/com/sparta/clonekurly/service/CustomUserDetailService.java
@@ -0,0 +1,25 @@
+package com.sparta.clonekurly.service;
+
+import com.sparta.clonekurly.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+
+@RequiredArgsConstructor
+@Service
+public class CustomUserDetailService implements UserDetailsService { //db에서 찾기
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+//        return userRepository.findByUsername(username)
+        return userRepository.findByEmail(username)
+                .orElseThrow(
+                () -> new UsernameNotFoundException("사용자를 찾을 수 없습니다.")
+        );
+    }
+}
